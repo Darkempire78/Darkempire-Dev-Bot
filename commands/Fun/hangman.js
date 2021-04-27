6613264cb7e4fdc9700d45266e79cdb7d264b226
Darkempire <>
2021-04-27 19:41:32 +0000

    updates

--- commands/Fun/hangman.js
+++ commands/Fun/hangman.js
@@ -1,27 +1,20 @@
 const { MessageEmbed } = require("discord.js");
 const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
+// const hangman = require('discord-hangman');
 
 module.exports = {
-    name: "ping",
-    category: "Information",
-    aliases: ["latency"],
+    name: "hangman",
+    category: "Fun",
+    aliases: ["hm"],
     cooldown: 2,
-    usage: "ping",
-    description: "Gives you information on how fast the Bot can respond to you",
+    usage: "hangman",
+    description: "Create a game of the Hangman",
     run: async (client, message, args, user, text, prefix) => {
         try{
-            message.channel.send(new MessageEmbed()
-                .setColor(ee.color)
-                .setFooter(ee.footertext, ee.footericon)
-                .setTitle(`🏓 Pinging....`)
-            ).then(msg=>{
-                msg.edit(new MessageEmbed()
-                .setColor(ee.color)
-                .setFooter(ee.footertext, ee.footericon)
-                .setTitle(`🏓 Ping is \`${Math.round(client.ws.ping)}ms\``)
-                );
-            })
+            // https://www.npmjs.com/package/discord-hangman
+            message.channel.send("soon")
+            // await hangman.create(message.channel, 'random')
         } catch (e) {
             console.log(String(e.stack).bgRed)
             return message.channel.send(new MessageEmbed()
