e75902e621bcced349995b62f356c519a0a0f8c7
Darkempire <>
2021-04-27 17:20:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const colors = require("colors");
+const fs = require("fs");
+
+//Creating the Discord.js Client for This Bot with some default settings
+const client = new Discord.Client({
+    messageCacheLifetime: 60,
+    fetchAllMembers: false,
+    messageCacheMaxSize: 10,
+    restTimeOffset: 0,
+    restWsBridgetimeout: 100,
+    disableEveryone: true,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+
+//Client variables to use everywhere
+client.commands = new Discord.Collection(); //an collection (like a digital map(database)) for all your commands
+client.aliases = new Discord.Collection(); //an collection for all your command-aliases
+client.categories = fs.readdirSync("./commands/"); //categories
+client.cooldowns = new Discord.Collection(); //an collection for cooldown commands of each user
+
+//Loading files, with the client variable like Command Handler, Event Handler, ...
+["command", "events"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+//login into the bot
+client.login(require("./botconfig/config.json").token);
