115cf1020cb2deaf64ffa78d5efbf5745bf9839d
Darkempire <>
2021-04-27 17:32:59 +0000

    a few small fixs

--- handlers/command.js
+++ handlers/command.js
@@ -2,7 +2,7 @@ const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
 let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
-console.log("Welcome to SERVICE HANDLER /--/ By https://milrato.eu /--/ Discord: Tomato#6966".yellow);
+
 module.exports = (client) => {
     try{
         readdirSync("./commands/").forEach((dir) => {
