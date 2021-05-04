cb65c977f9cdd131d1adf698551008908b85c4e9
Darkempire78 <clement.wawszczyk@gmail.com>
2021-05-04 19:16:50 +0200

    new command : snake

--- commands/Fun/snake.js
+++ commands/Fun/snake.js
@@ -1,19 +1,26 @@
 const { MessageEmbed } = require("discord.js");
 const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
-const hangman = require('discord-hangman');
+const snake = require("snakecord");
 
 module.exports = {
-    name: "hangman",
+    name: "snake",
     category: "Fun",
-    aliases: ["hm"],
+    aliases: [],
     cooldown: 2,
-    usage: "hangman",
-    description: "Create a game of the Hangman game",
+    usage: "snake",
+    description: "Create a game of the Snake game",
     run: async (client, message, args, user, text, prefix) => {
         try{
-            // https://www.npmjs.com/package/discord-hangman
-            await hangman.create(message.channel, 'random')
+            // https://www.npmjs.com/package/snakecord
+            const snakeGame = new snake({
+                title: "Snake Game",
+                color: "BLUE",
+                timestamp: true,
+                gameOverTitle: "You win!"
+                    })
+    
+            snakeGame.newGame(message)
         } catch (e) {
             console.log(String(e.stack).bgRed)
             return message.channel.send(new MessageEmbed()
