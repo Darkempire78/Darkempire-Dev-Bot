d473f8346f7e75407951f7fc5153ef25d67621ff
Darkempire78 <clement.wawszczyk@gmail.com>
2021-06-13 00:42:41 +0200

    fix member count

--- commands/Fun/snake.js
+++ commands/Fun/snake.js
@@ -1,34 +1,34 @@
-const { MessageEmbed } = require("discord.js");
-const config = require("../../botconfig/config.json");
-const ee = require("../../botconfig/embed.json");
-const snake = require("snakecord");
+// const { MessageEmbed } = require("discord.js");
+// const config = require("../../botconfig/config.json");
+// const ee = require("../../botconfig/embed.json");
+// const snake = require("snakecord");
 
-module.exports = {
-    name: "snake",
-    category: "Fun",
-    aliases: [],
-    cooldown: 2,
-    usage: "snake",
-    description: "Create a game of the Snake game",
-    run: async (client, message, args, user, text, prefix) => {
-        try{
-            // https://www.npmjs.com/package/snakecord
-            const snakeGame = new snake({
-                title: "Snake Game",
-                color: "BLUE",
-                timestamp: true,
-                gameOverTitle: "You win!"
-                    })
+// module.exports = {
+//     name: "snake",
+//     category: "Fun",
+//     aliases: [],
+//     cooldown: 2,
+//     usage: "snake",
+//     description: "Create a game of the Snake game",
+//     run: async (client, message, args, user, text, prefix) => {
+//         try{
+//             // https://www.npmjs.com/package/snakecord
+//             const snakeGame = new snake({
+//                 title: "Snake Game",
+//                 color: "BLUE",
+//                 timestamp: true,
+//                 gameOverTitle: "You win!"
+//                     })
     
-            snakeGame.newGame(message)
-        } catch (e) {
-            console.log(String(e.stack).bgRed)
-            return message.channel.send(new MessageEmbed()
-                .setColor(ee.wrongcolor)
-                .setFooter(ee.footertext, ee.footericon)
-                .setTitle(`❌ ERROR | An error occurred`)
-                .setDescription(`\`\`\`${e.stack}\`\`\``)
-            );
-        }
-    }
-}
+//             snakeGame.newGame(message)
+//         } catch (e) {
+//             console.log(String(e.stack).bgRed)
+//             return message.channel.send(new MessageEmbed()
+//                 .setColor(ee.wrongcolor)
+//                 .setFooter(ee.footertext, ee.footericon)
+//                 .setTitle(`❌ ERROR | An error occurred`)
+//                 .setDescription(`\`\`\`${e.stack}\`\`\``)
+//             );
+//         }
+//     }
+// }
